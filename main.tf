c6292be2face3d33b940bfaf8138e7ddf32741e8
Adam Schumacher <adam@genericor.com>
2024-10-21 19:01:30 +0000

    Initial

---
+++ main.tf
@@ -0,0 +1,145 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.56"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_rt_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.ssh_key.private_key_pem
+  filename = "./.ssh/terraform_rsa"
+}
+
+resource "local_file" "public_key" {
+  content  = tls_private_key.ssh_key.public_key_openssh
+  filename = "./.ssh/terraform_rsa.pub"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "ubuntu_ssh_key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+resource "aws_security_group" "allow_ssh_http_https" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-ssh-https-8080"
+  }
+}
+
+resource "aws_instance" "ubuntu_instance" {
+  count                       = 1
+  ami                         = "ami-0a0e5d9c7acc336f1"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.allow_ssh_http_https.id]
+  key_name                    = aws_key_pair.deployer.key_name
+  associate_public_ip_address = true
+
+  depends_on = [
+    aws_security_group.allow_ssh_http_https,
+    aws_internet_gateway.igw
+  ]
+
+  user_data       = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo sed -i 's/^#.*WaylandEnable=.*/WaylandEnable=false/' /etc/gdm3/custom.conf
+
+              # sudo apt install -y nginx
+
+              # # Create index.html with H1 tag in the default NGINX web directory
+              # echo "<h1>Hello From Ubuntu EC2 Instance!!!</h1>" | sudo tee /var/www/html/index.html
+
+              # # Update NGINX to listen on port 8080
+              # sudo sed -i 's/listen 80 default_server;/listen 8080 default_server;/g' /etc/nginx/sites-available/default
+
+              # # Restart NGINX to apply the changes
+              # sudo systemctl restart nginx
+              EOF
+
+  tags = {
+    Name = "${format("jumphost-%03d", count.index + 1)}"
+  }
+}
