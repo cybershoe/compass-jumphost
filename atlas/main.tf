6240e91283553907d15ca69072cd71446529c5ac
Adam Schumacher <adam@genericor.com>
2025-07-15 15:07:43 -0400

    Add sample dataset provisioner

---
+++ atlas/main.tf
@@ -0,0 +1,65 @@
+# provider "mongodbatlas" {
+#   # public_key = var.public_key
+#   # private_key  = var.private_key
+# }
+
+resource "mongodbatlas_advanced_cluster" "lab_cluster" {
+  count = var.replicas
+  project_id   = var.project_id
+  name         = "${var.prefix}${format("-%03d", count.index+1)}"
+  cluster_type = "REPLICASET"
+  replication_specs {
+    region_configs {
+      electable_specs {
+        instance_size = "M10"
+        node_count    = 3
+      }
+      provider_name = "AWS"
+      priority      = 7
+      region_name   = "US_EAST_1"
+    }
+  }
+
+  tags {
+    key   = "owner"
+    value = var.owner
+  }
+
+  provisioner "local-exec" {
+    command = "atlas clusters sampleData load ${self.name} --projectId ${var.project_id}"
+  }
+}
+
+resource "mongodbatlas_project_ip_access_list" "local_ip" {
+  count = var.replicas
+  project_id = var.project_id
+  ip_address = var.jumphosts[count.index].ip
+  comment    = "ip address for access from jumphost"
+}
+
+resource "mongodbatlas_database_user" "test" {
+  count              = var.replicas  
+  username           = var.jumphosts[count.index].username
+  password           = var.jumphosts[count.index].password
+  project_id         = var.project_id
+  auth_database_name = "admin"
+
+  roles {
+    role_name     = "atlasAdmin"
+    database_name = "admin"
+  }
+
+  labels {
+    key   = "owner"
+    value = var.owner
+  }
+
+  scopes {
+    name   = mongodbatlas_advanced_cluster.lab_cluster[count.index].name
+    type = "CLUSTER"
+  }
+}
+
+output connection_strings {
+  value = mongodbatlas_advanced_cluster.lab_cluster[*].connection_strings[0].standard_srv
+}
---
+++ atlas/vars.tf
@@ -0,0 +1,50 @@
+# variable public_key {
+#   type        = string
+#   description = "MongoDB Atlas Public Key"
+# }
+
+# variable private_key {
+#   type        = string
+#   description = "MongoDB Atlas Private Key"
+#   sensitive   = true
+# }
+
+variable project_id {
+  type        = string
+  description = "MongoDB Atlas Project ID"
+}
+
+variable prefix {
+  type        = string
+  description = "Prefix for MongoDB Atlas cluster names"
+}
+
+variable owner {
+  type        = string
+  description = "Cluster owner tag"
+}
+
+variable replicas {
+  type       = number
+  description = "Number of clusters to deploy"
+}
+
+# variable jumphost_ips {
+#   type        = list(string)
+#   description = "List of jumphost IPs to allow access to the MongoDB Atlas cluster"
+# }
+
+# variable passwords {
+#   type        = list(string)
+#   description = "List of passwords for the atlas users"
+# }
+
+variable jumphosts {
+  type          = list(object({
+    username      = string
+    password      = string
+    ip            = string
+    url           = string
+  }))
+  description = "Map of jumphost instance IDs to their passwords"
+}
